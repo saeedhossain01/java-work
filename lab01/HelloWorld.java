d6b1cde2c8e8d28ef22a425a24d3e7f1efecf45b
saeedhossain01 <saeedhossain01@gmail.com>
2014-11-27 19:07:00 +0000

    updated HelloWorld

---
+++ lab01/HelloWorld.java
@@ -0,0 +1,24 @@
+//Programmer: Saeed Hossain
+//Program name: HelloWorld
+//Purpose: This is an introduction to programming
+
+//  first compile the program
+//      javac HelloWorld.java
+//  run the program
+//      java HelloWorld//
+
+//  difine a class
+public class HelloWorld{
+    
+//  add main method
+  public static void main(String[] args) {
+
+//  print the statement with box around it
+    System.out.println("########################");
+    System.out.println("#      HelloWorld      #");        
+    System.out.println("########################");
+    System.out.println("test");
+    
+    //test/testtest
+  }
+}  
