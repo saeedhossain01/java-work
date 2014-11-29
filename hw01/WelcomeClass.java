4d30a58c632b2f9373b8652c73f0b66fbc9889e9
saeedhossain01 <saeedhossain01@gmail.com>
2014-11-29 02:57:19 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,17 @@
+//Programmer: Saeed Hossain
+//Program: WelcomeClass
+//Purpose: Homework
+
+public class WelcomeClass{
+    
+  public static void main(String[] arges) {
+      
+    System.out.println("-----------");
+    System.out.println("| Welcome |");
+    System.out.println("-----------");
+    System.out.println("  ^  ^  ^  ^  ^  ");
+    System.out.println("|-S--A--E--E--D-|");
+    System.out.println("  v  v  v  v  v  ");
+    
+  }
+}
